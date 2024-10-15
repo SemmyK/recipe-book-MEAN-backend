0da4e75350972f898cd6f66ce7f1b3e511fd1fce
SemmyK <66922979+SemmyK@users.noreply.github.com>
2024-10-15 17:24:20 +0200

    adding new url to cors policy

--- src/server.ts
+++ src/server.ts
@@ -21,6 +21,7 @@ connectToDatabase(ATLAS_URI)
 		const app = express()
 		const allowedOrigins = [
 			'https://frontend-3l8g70u1e-semmyks-projects.vercel.app/',
+			'https://frontend-omega-nine-87.vercel.app/',
 			'http://localhost:4200',
 		]
 
