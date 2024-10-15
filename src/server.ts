66dfb7e92a92c70084a1e98e1499ad6870af49a9
SemmyK <66922979+SemmyK@users.noreply.github.com>
2024-10-15 15:30:56 +0200

    update backend logic for cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv'
 import express from 'express'
-import cors from 'cors'
+import cors, { CorsOptions } from 'cors'
 import { connectToDatabase } from '../config/database'
 import { recipeRouter } from '../routes/recipe.routes'
 
@@ -19,15 +19,31 @@ if (!ATLAS_URI) {
 connectToDatabase(ATLAS_URI)
 	.then(() => {
 		const app = express()
-		app.use(cors())
-		app.use(
-			cors({
-				origin: '*',
-			})
-		)
+		const allowedOrigins = [
+			'https://recipe-book-mean-frontend-semmyk-semmyks-projects.vercel.app',
+			'https://recipe-book-mean-frontend-jcvaiox2a-semmyks-projects.vercel.app/',
+			'http://localhost:4200',
+		]
+
+		const corsOptions: CorsOptions = {
+			origin: (origin, callback) => {
+				if (!origin) return callback(null, true)
+				if (allowedOrigins.includes(origin)) {
+					callback(null, true)
+				} else {
+					callback(new Error('Not allowed by CORS'))
+				}
+			},
+			credentials: true,
+		}
+
+		app.use(cors(corsOptions))
+
 		app.use(express.json())
 		app.use('/api/recipes', recipeRouter)
-
+		app.get('/api/recipes', (req, res) => {
+			res.json({ message: 'This is your API response' })
+		})
 		app.listen(PORT, () => {
 			console.log(`Server running at http://localhost:${PORT}...`)
 		})
